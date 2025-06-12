7f869d804aa79264668e5428bbd50370e7cbdce7
pradeepkudukkil <iampradeepk0@gmail.com>
2025-06-12 18:08:20 +0530

    initial commit

--- autoFixer.py
+++ autoFixer.py
@@ -3,10 +3,10 @@ from huggingface_hub import InferenceClient
 
 client = InferenceClient(
     provider="together",
-    api_key=os.environ.get("HF_TOKEN")
+    api_key=os.environ["HF_TOKEN"]
 )
 
-error_log = os.environ.get("ERROR_MESSAGE", "No error provided")
+error_log = os.environ.get("ERROR_MESSAGE", "No error message received.")
 
 completion = client.chat.completions.create(
     model="mistralai/Mistral-7B-Instruct-v0.3",
